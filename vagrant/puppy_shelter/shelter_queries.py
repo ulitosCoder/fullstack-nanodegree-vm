35b50cb879f8a9896020b3b770ce9c193dc97364
U-AMI_NT\alvarau <alvarau@slp-4nr7nx1.mcquay.com>
2016-05-26 11:36:21 -0500

    finished exercises

--- vagrant/puppy_shelter/shelter_queries.py
+++ vagrant/puppy_shelter/shelter_queries.py
@@ -1,3 +1,4 @@
+#!/bin/python
 from sqlalchemy import create_engine, desc, asc, Date
 from sqlalchemy.orm import sessionmaker
 import datetime
@@ -19,19 +20,23 @@ for item in query1:
 	print item.name #, ', ',  item.dateOfBirth
 
 #2. Query all of the puppies that are less than 6 months old organized by the youngest firs
-today = datetime.datetime.today()
 
-print today
-#query2 = session.query(Puppy).all()
-#for item in query2:
-	#print item.name
+today = datetime.datetime.now().date()
+
+sixMonths = datetime.timedelta(days=(6*30))
+sixMonthsAgo = today-sixMonths
+
+query2 = session.query(Puppy).filter( Puppy.dateOfBirth > sixMonthsAgo ).order_by(desc(Puppy.dateOfBirth)).all()
+for item in query2:
+	
+	print item.name , ', ', item.dateOfBirth
 
 #3. Query all puppies by ascending weight
 query3 = session.query(Puppy).order_by(asc(Puppy.weight)).all()
-for item in query3:
-	print item.name , ', ',  item.weight
+#for item in query3:
+#	print item.name , ', ',  item.weight
 
 #4. Query all puppies grouped by the shelter in which they are staying
 query4 = session.query(Puppy).order_by(asc(Puppy.shelter_id)).all()
-for item in query4:
-	print item.name , ', ',  item.shelter_id
+#for item in query4:
+#	print item.name , ', ',  item.shelter_id
