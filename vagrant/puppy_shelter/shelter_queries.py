56c503c5085584bf2de60031405fcc946a1d3b85
ulises <ulisesaj83@gmail.com>
2016-05-26 00:43:12 -0500

    :prog :wq

--- vagrant/puppy_shelter/shelter_queries.py
+++ vagrant/puppy_shelter/shelter_queries.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, desc, asc, Date
 from sqlalchemy.orm import sessionmaker
- 
+import datetime
 from puppies import Base, Shelter, Puppy
 
 engine = create_engine('sqlite:///puppyshelter.db')
@@ -14,6 +14,24 @@ session = DBSession()
 
 #1. Query all of the puppies and return the results in ascending alphabetical order
 
-query1 = session.query(Puppy).all()
+query1 = session.query(Puppy).order_by(asc(Puppy.name)).all()
 for item in query1:
-	print item.name
+	print item.name #, ', ',  item.dateOfBirth
+
+#2. Query all of the puppies that are less than 6 months old organized by the youngest firs
+today = datetime.datetime.today()
+
+print today
+#query2 = session.query(Puppy).all()
+#for item in query2:
+	#print item.name
+
+#3. Query all puppies by ascending weight
+query3 = session.query(Puppy).order_by(asc(Puppy.weight)).all()
+for item in query3:
+	print item.name , ', ',  item.weight
+
+#4. Query all puppies grouped by the shelter in which they are staying
+query4 = session.query(Puppy).order_by(asc(Puppy.shelter_id)).all()
+for item in query4:
+	print item.name , ', ',  item.shelter_id
