8e7ffd86d9af0920ba77c37bbc1a5df8b85f724f
ulitosCoder <ulisesaj83@gmail.com>
2016-08-16 10:53:30 -0500

    Added commentos to database_setup.py Started README.md

--- vagrant/catalog/README.md
+++ vagrant/catalog/README.md
@@ -1,1 +1,24 @@
-Place your catalog project in this directory.
+# Udacity's Web developer full stack Project 2 - Items Catalog
+==============================================================
+
+This project stores information about different categories and items
+whithin each category. Implements security with Authentication 
+and authorization with third party authentication providers.
+
+
+
+## Install
+------------
+
+
+The easiest way to test this code is with [Vagrant](https://www.vagrantup.com/)
+
+Follow these steps to get you started:
+
+1. Install [Vagrant](https://www.vagrantup.com/)
+2. Install [VirtualBox](https://www.virtualbox.org/)
+3. Install [cygwin](https://www.cygwin.com/), be sure to select **git** from the packages list 
+
+---------------
+
+## Setting up the environment
--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -5,24 +5,38 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
 
+
 class User(Base):
+
+    """
+        Defines a user fo the wesite
+
+        This class contains the name, email and url for the picture of the 
+        loged user.
+    """
     __tablename__ = 'user'
-    id          = Column(Integer, primary_key = True)
-    name        = Column(String(80), nullable = False)
-    email       = Column(String(80), nullable = True)
-    picture     = Column(String(80), nullable = True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    email = Column(String(80), nullable=True)
+    picture = Column(String(80), nullable=True)
 
 
 class Category(Base):
-    __tablename__   = 'category'
-    id              = Column(Integer, primary_key = True)
-    name            = Column(String(80), nullable = False)
-    user_id         = Column(Integer,ForeignKey('user.id'))
-    
-    user            = relationship(User)
+    """ Defines a category of items
+        
+        This class contains the Name of the category, ID and the Id of the
+        user that created the category. Also implements the serialization
+        of a JSON string for the class.
+    """
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    user_id = Column(Integer, nullable=False, ForeignKey('user.id'))
+
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -33,17 +47,24 @@ class Category(Base):
             'user_id': self.user_id,
         }
 
+
 class CategoryItem(Base):
-    __tablename__   = 'category_item'
-    id              = Column(Integer, primary_key = True)
-    name            = Column(String(80), nullable = False)
-    date_added      = Column(DateTime, default=func.now())
-    description     = Column(Text)
-    category_id     = Column(Integer, ForeignKey('category.id'))
-    user_id         = Column(Integer,ForeignKey('user.id'))
-    
-    category        = relationship(Category)
-    user            = relationship(User)
+    """ Defines an item that belongs to a  category.
+        
+        This class contains the Name of the item, ID, the IDs of the
+        user that created the item and the ID of the category this item belongs
+        to. Also implements the serialization of a JSON string for the class.
+    """
+    __tablename__ = 'category_item'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    date_added = Column(DateTime, default=func.now())
+    description = Column(Text)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    category = relationship(Category)
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -53,7 +74,7 @@ class CategoryItem(Base):
             'id': self.id,
             'user_id': self.user_id,
             'category_id': self.category_id,
-            'date_added' : self.date_added
+            'date_added': self.date_added
         }
 
 
