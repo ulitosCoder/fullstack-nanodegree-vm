c1ecfec5ec0958d83848cecfcc73b664522154b1
ulitosCoder <ulisesaj83@gmail.com>
2016-08-16 11:33:46 -0500

    bug removed

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -34,7 +34,7 @@ class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
-    user_id = Column(Integer, nullable=False, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
 
     user = relationship(User)
 
